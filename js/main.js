2e1bc31c92d967a4e7505fdc65a0ccd29720750b
Christopher Harding <chrisharding88@gmail.com>
2017-01-11 22:32:12 -0500

    additional change w/ home page, amenities page, & contact page

--- js/main.js
+++ js/main.js
@@ -1,40 +1,71 @@
-//to ensure the DOM is ready 
-$('.slider').each(function(){
-	var $this = $(this);
-	var $group = $this.find('.slide-group');
-	var $slides = $this.find('.slide');
-	var buttonArray =[];
-	var currentIndex = 0;
-	var timeout;
+	//to ensure the DOM is ready 
+$(document).on('ready', function(){
 
-	function advance () {
-		clearTimeout(timeout);
-		//start timer to run an anonymous function every 4 seconds
-		timeout = setTimeout (function() {
-			if (currentIndex < ($slides.length - 1)){
-				move(currentIndex + 1);
-			} else {
-			   move(0);
-			}
+	/* var count = 0;
+	var images = ["../img/indoor-pool.jpg","../img/welcomecenter.jpg","../img/game-room.jpg","../img/room1.jpg"];
+	var img_background = $("html.background");
 
-	  	}, 4000)
-	}
+	img_background.css("background","("+images[count]+")")
 
-	$.each($slides, function (index){
-		//create a button element for the button
-		var button = $('<button type="button" class="slide-btn">&bull,</button>');
-		if (index === currentIndex) {
-			$button.addClass('active');
+	setInterval(function(){
+		img_background.fadeOut(500, function(){
+			img_background.css("background","("+images[count++]+")");
+			img_background.fadeIn(500);
+		});
+		if (count === img_background.length)
+		{
+			count = 0;
 		}
-		$button.on('click', function() {
-			move(index);
-		}).appendTo($this.find('.slide-buttons'));
-		buttonArray.push($button);
-	});
 
-	advance();	//Script is set up, call advance to move it
+	}, 5000);*/
 
 
 
 
-}); //concludes the slide
+
+
+
+
+
+	$('.slider').each(function(){
+		var $this = $(this);
+		var $group = $this.find('.slide-group');
+		var $slides = $this.find('.slide');
+		var buttonArray = [];
+		var currentIndex = 0;
+		var timeout;
+
+		function advance () {
+			clearTimeout(timeout);
+			//start timer to run an anonymous function every 4 seconds
+			timeout = setTimeout (function() {
+				if (currentIndex < ($slides.length - 1)){
+					move(currentIndex + 1);
+				} else {
+				   move(0);
+				}
+
+		  	}, 4000)
+		}
+
+		$.each($slides, function (index){
+			var foo = $this;
+			//create a button element for the button
+			var $button = $('<button type="button" class="slide-btn">&bull,</button>');
+			if (index === currentIndex) {
+				$button.addClass('active');
+			}
+			$button.on('click', function() {
+				move(index);
+			}).appendTo($this.find('.slide-buttons'));
+			buttonArray.push($button);
+		});
+
+		advance();	//Script is set up, call advance to move it
+
+
+
+
+	}); //concludes the slide
+
+}); // closes doc.ready
